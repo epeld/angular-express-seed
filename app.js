d45f195db7267c198bec9ef0fc3824d623c10272
Erik Peldan <epe@trifork.com>
2016-10-09 14:44:00 +0200

    Fix dependencies so project runs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  errorHandler = require('error-handler'),
+  errorHandler = require('errorhandler'),
   morgan = require('morgan'),
   routes = require('./routes'),
   api = require('./routes/api'),
@@ -25,15 +25,15 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
-app.use(bodyParser());
+app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var env = process.env.NODE_ENV || 'development';
 
 // development only
-if (env === 'development') {
-  app.use(express.errorHandler());
+if (true || env === 'development') {
+    app.use(errorHandler());
 }
 
 // production only
